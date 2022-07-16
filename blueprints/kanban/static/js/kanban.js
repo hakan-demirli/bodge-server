82f384688796ca98e1e9ddbd76d75b0fb36e2beb
hakan-demirli <emre.demirli@metu.edu.tr>
2022-07-16 21:54:01 +0300

    add dummy plot to sp-view
    add c3 and d3 libs

--- blueprints/kanban/static/js/kanban.js
+++ blueprints/kanban/static/js/kanban.js
@@ -1013,4 +1013,14 @@ $(function () {
         selectCard(kanban_data.projects_accessible['id'][id][1]);
         selectCard(kanban_data.projects_accessible['id'][id][2]);
     });
+
+    var chart = c3.generate({
+        bindto: '#chart',
+        data: {
+          columns: [
+            ['data1', 30, 200, 100, 400, 150, 250],
+            ['data2', 50, 20, 10, 40, 15, 25]
+          ]
+        }
+    });
 })
