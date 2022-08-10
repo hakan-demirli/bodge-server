83102a3cb78116550352c4ca59f1a2ea69f317df
hakan-demirli <emre.demirli@metu.edu.tr>
2022-08-11 01:31:27 +0300

    add configurable remote repo url

--- blueprints/settings/bp.py
+++ blueprints/settings/bp.py
@@ -24,7 +24,7 @@ class MyBlueprint():
 
     def __init_user_data(self):
         if(not os.path.isfile(self.user_data_file_path)):
-            dummy_json = {}
+            dummy_json = {'git_repo_url': ""}
             with open(self.user_data_file_path, 'w') as outfile:
                 json.dump(dummy_json, outfile, indent=4)
 
@@ -37,22 +37,30 @@ class MyBlueprint():
             jsn_res = {}
             match req['command']:
                 case 'CHANGE_REMOTE':
-                    if(os.path.exists(self.user_data_folder)):
-                        shutil.rmtree(self.user_data_folder)
+                    info = subprocess.run(['git', 'ls-remote', req['git_repo_url']])
+                    if(info.returncode != 0):
+                        print("ERROR CANT FIND THE REPO")
                     else:
-                        print("sad")
-                case 'PULL':
+                        if(os.path.exists(self.user_data_folder_path)):
+                            shutil.rmtree(self.user_data_folder_path)
+                            Path(self.user_data_folder_path).mkdir(parents=True)
+                            info = subprocess.run(['git', '-C', str(self.user_data_folder_path), 'clone', req['git_repo_url'], '.'])
+                            with open(self.user_data_file_path, 'w') as outfile:
+                                json.dump(req, outfile, indent=4)
+                        else:
+                            print("ERROR CANT FIND DATA FOLDER")
+                case 'SYNC_REPO':
+                    info = subprocess.run(['git', '-C', str(self.user_data_folder_path),'pull'])
+                    print(info)
+                    info = subprocess.run(['git', '-C', str(self.user_data_folder_path),'add','.'])
+                    print(info)
+                    info = subprocess.run(['git', '-C', str(self.user_data_folder_path),'commit','-m', str(datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S"))])
+                    print(info)
+                    info = subprocess.run(['git', '-C', str(self.user_data_folder_path),'push'])
+                    print(info)
+                case 'READ':
                     with open(self.user_data_file_path) as json_file:
                         jsn_res = json.load(json_file)
-                case 'PUSH':
-                    info = subprocess.run(['git', '-C', str(self.user_data_folder),'init'])
-                    print(info)
-                    info = subprocess.run(['git', '-C', str(self.user_data_folder),'pull'])
-                    print(info)
-                    info = subprocess.run(['git', '-C', str(self.user_data_folder),'add','.'])
-                    print(info)
-                    info = subprocess.run(['git', '-C', str(self.user_data_folder),'commit','-m', str(datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S"))])
-                    print(info)
-                    info = subprocess.run(['git', '-C', str(self.user_data_folder),'push'])
-                    print(info)
+                case default:
+                    raise Exception("UNKNOWN COMMAND")
             return make_response(jsonify(jsn_res), 200)
--- blueprints/settings/static/js/settings.js
+++ blueprints/settings/static/js/settings.js
@@ -1,17 +1,34 @@
 
 $(function () {
 
+var padlock_state = 0;
 $('#repo-submit').on('click',function(e){
+
     console.log('hi');
-    settingsWriteBackend();
+    $('#repo-lock').toggleClass('fa-lock');
+    $('#repo-lock').toggleClass('fa-lock-open');
+    if(padlock_state) {
+        $("#git-repo").attr("disabled", "");
+        let entry = {
+            command: 'CHANGE_REMOTE',
+            git_repo_url: $("#git-repo").val()
+        };
+        settingsWriteBackend(entry)
+    }else{
+        $("#git-repo").removeAttr("disabled");
+    }
+
+    padlock_state = !padlock_state;
 });
 
-
-function settingsWriteBackend() {
-
+$('#repo-sync').on('click',function(e){
     let entry = {
-        command: 'PUSH'
+        command: 'SYNC_REPO'
     };
+    settingsWriteBackend(entry)
+});
+
+function settingsWriteBackend(entry) {
 
     fetch(`${window.origin}/settings/backend`, {
         method: "POST",
@@ -30,4 +47,37 @@ function settingsWriteBackend() {
         console.log("Fetch error: " + error);
     });
 }
+
+function settingsReadBackend(entry) {
+
+    fetch(`${window.origin}/settings/backend`, {
+        method: "POST",
+        credentials: "include",
+        body: JSON.stringify(entry),
+        cache: "no-cache",
+        headers: new Headers({"content-type": "application/json"})
+    }).then(function (response) {
+        if (response.status !== 200) {
+            console.log(`Looks like there was a problem. Status code: ${response.status}`);
+            return;
+        }
+        response.json().then(function (data) {
+            $("#git-repo").val(data['git_repo_url'])
+        });
+    })
+    .catch(function (error) {
+        console.log("Fetch error: " + error);
+    });
+}
+
+window.onerror = function(e){
+    document.getElementById('error-log').innerHTML = e.toString();
+}
+
+let entry = {
+    command: 'READ'
+};
+
+settingsReadBackend(entry)
+
 });
